6aedd5d48f85da58c0ff0d5adecd8cafbaacc51f
NyondoPaul <nyondopaul23@gmail.com>
2017-07-03 17:47:01 +0300

    code refactor

--- primenumber.py
+++ primenumber.py
@@ -9,14 +9,10 @@ def primenumber(number_range):
     """ Returns  a  list of prime numbers using sieve of erastothenes """
     checktype(number_range)
     number = 2
-    output = []
     prime = [True for num in range(number_range+1)]
     while number*number <= number_range:
         if prime[number]:
             for num2 in range(number*2, number_range+ 1, number):
                 prime[num2] = False
         number += 1
-    for num in range(2, number_range):
-        if prime[num]:
-            output.append(num)
-    return output
+    return [num for num in range(2, number_range) if prime[num]]
