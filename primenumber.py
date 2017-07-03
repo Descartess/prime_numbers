d55e0d8653f44ce7a084099e3661d54d76d72ac3
NyondoPaul <nyondopaul23@gmail.com>
2017-07-03 13:57:04 +0300

    Added enough code forr tests to fail

---
+++ primenumber.py
@@ -0,0 +1,8 @@
+""" finctions to compute """
+def checktype(datatype):
+    """ checks data type of arguments """
+    pass
+
+def primenumber(number_range):
+    """ Returns  a  list of prime numbers """
+    pass
