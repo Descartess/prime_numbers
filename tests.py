f682006e1081e45af636a6ca256f295773e00902
NyondoPaul <nyondopaul23@gmail.com>
2017-07-03 13:47:25 +0300

    Added test to check prime number in range(10)

--- tests.py
+++ tests.py
@@ -6,6 +6,10 @@ class PrimeNumberTest(unittest.TestCase):
     """ tests the prime number functions """
     def testdatatype(self):
         """checks that appropriate data type is supplied"""
-        args = ["", " ", -1, -1000,"food"]
+        args = ["", " ", -1, -1000, "food"]
         self.assertRaises(TypeError, checktype, *args)
+    def testprimenumbers(self):
+        """ checks prime numbers under 10"""
+        prime_number_list = primenumber(10)
+        self.assertEqual(prime_number_list, [2, 3, 5, 7])
 
